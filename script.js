6fc9823334b5343bb56192b25c01e78bb380d0bb
Evgeniia <kuznetsovaevgeni@gmail.com>
2023-10-28 17:33:24 +0300

    Добавила скрипт, бургер-меню

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let button = document.querySelector('.burger');
-let close = document.querySelector('.close');
-let mobile_menu = document.querySelector('.mobile_menu');
+let close = document.querySelector('.close-btn');
+let mobile = document.querySelector('.mobile');
 let navHome = document.querySelector(".navHome");
 let navAbout = document.querySelector(".navAbout");
 let navSkills = document.querySelector(".navSkills");
@@ -8,25 +8,26 @@ let navPortfolio = document.querySelector(".navPortfolio");
 let navContacts = document.querySelector(".navContacts");
 
 button.addEventListener('click', function() {
-    mobile_menu.classList.remove('d-none');
+    mobile.classList.remove('d-none');
 })
 
 close.addEventListener('click', function() {
-    mobile_menu.classList.add('d-none');
+    mobile.classList.add('d-none');
 })
 
 navHome.addEventListener('click', function() {
-    mobile_menu.classList.add('d-none');
+    mobile.classList.add('d-none');
 })
 navAbout.addEventListener('click', function() {
-    mobile_menu.classList.add('d-none');
+    mobile.classList.add('d-none');
 })
 navSkills.addEventListener('click', function() {
-    mobile_menu.classList.add('d-none');
+    mobile.classList.add('d-none');
 })
 navPortfolio.addEventListener('click', function() {
-    mobile_menu.classList.add('d-none');
+    mobile.classList.add('d-none');
 })
 navContacts.addEventListener('click', function() {
-    mobile_menu.classList.add('d-none');
+    mobile.classList.add('d-none');
 })
+ 
