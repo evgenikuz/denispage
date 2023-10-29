12ccfc680b4e7174a4966b591d23e624e8702a8a
Evgeniia <kuznetsovaevgeni@gmail.com>
2023-10-29 10:19:06 +0300

    мягкое прокручивание и облегченный скрипт

--- script.js
+++ script.js
@@ -1,33 +1,40 @@
 let button = document.querySelector('.burger');
-let close = document.querySelector('.close-btn');
+// let close = document.querySelector('.close-btn');
 let mobile = document.querySelector('.mobile');
-let navHome = document.querySelector(".navHome");
-let navAbout = document.querySelector(".navAbout");
-let navSkills = document.querySelector(".navSkills");
-let navPortfolio = document.querySelector(".navPortfolio");
-let navContacts = document.querySelector(".navContacts");
+// let navHome = document.querySelector(".navHome");
+// let navAbout = document.querySelector(".navAbout");
+// let navSkills = document.querySelector(".navSkills");
+// let navPortfolio = document.querySelector(".navPortfolio");
+// let navContacts = document.querySelector(".navContacts");
+let item = document.querySelectorAll('.item');
 
 button.addEventListener('click', function() {
     mobile.classList.remove('d-none');
 })
 
-close.addEventListener('click', function() {
-    mobile.classList.add('d-none');
+// close.addEventListener('click', function() {
+//     mobile.classList.add('d-none');
+// })
+
+item.forEach(el => {
+    el.addEventListener('click', function() {
+        mobile.classList.add('d-none');
+    })
 })
 
-navHome.addEventListener('click', function() {
-    mobile.classList.add('d-none');
-})
-navAbout.addEventListener('click', function() {
-    mobile.classList.add('d-none');
-})
-navSkills.addEventListener('click', function() {
-    mobile.classList.add('d-none');
-})
-navPortfolio.addEventListener('click', function() {
-    mobile.classList.add('d-none');
-})
-navContacts.addEventListener('click', function() {
-    mobile.classList.add('d-none');
-})
+// navHome.addEventListener('click', function() {
+//     mobile.classList.add('d-none');
+// })
+// navAbout.addEventListener('click', function() {
+//     mobile.classList.add('d-none');
+// })
+// navSkills.addEventListener('click', function() {
+//     mobile.classList.add('d-none');
+// })
+// navPortfolio.addEventListener('click', function() {
+//     mobile.classList.add('d-none');
+// })
+// navContacts.addEventListener('click', function() {
+//     mobile.classList.add('d-none');
+// })
  
